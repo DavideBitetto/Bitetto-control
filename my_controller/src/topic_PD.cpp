d8984ba15f60c9317544162b1851da9e760e0dda
d.bitetto <d.bitetto@studenti.unipi.it>
2021-04-20 19:50:16 +0200

    traiettoria

--- my_controller/src/topic_PD.cpp
+++ my_controller/src/topic_PD.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include <string>
+#include <math.h>
 #include <ros/ros.h>
+#include <ros/time.h>
 #include <sensor_msgs/JointState.h>
 #include <robot_state_publisher/robot_state_publisher.h>
 
@@ -15,27 +17,49 @@ int main(int argc, char** argv)
     
     // message declarations
     sensor_msgs::JointState joint_state;
+
+    joint_state.name.resize(2);
+    joint_state.position.resize(2);
+    joint_state.velocity.resize(2);
+    joint_state.effort.resize(2);
+    
+    double t_f = 0.05;
+    double k = 0.0;
+    
+
+    double sec = ros::Time::now().toSec();    
+    double t = t_f + sec;
     
     while (ros::ok()) {
-        joint_state.name.resize(2);
-        joint_state.position.resize(2);
-        joint_state.velocity.resize(2);
-        joint_state.effort.resize(2);
-        
+
+        while((ros::Time::now().toSec() - t < 0) && (k < 6.3)) {
+        ros::spinOnce();
+
+        cout << "sono nel while:   " << ros::Time::now().toSec() <<  endl;
         joint_state.name[0]="joint_1";
-        joint_state.position[0] = 0.0;
+        joint_state.position[0] = k;
         joint_state.velocity[0] = 0.0;
         joint_state.effort[0] = 0.0;
         joint_state.name[1] ="joint_2";
-        joint_state.position[1] = 1.5;
+        joint_state.position[1] = k;
         joint_state.velocity[1] = 0.0;
         joint_state.effort[0] = 0.0;
-
-        //send the joint state and transform
         joint_pub.publish(joint_state);
+        cout << "posizione:   " <<joint_state.position[0] << endl;
+        loop_rate.sleep();
+        }
+        
+        cout << "sono uscitooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo:   " << ros::Time::now().toSec() << endl;
+        
+        k+= 0.05;
+        
+        sec = ros::Time::now().toSec(); 
+        
+        t = t_f + sec;
 
         // This will adjust as needed per iteration
-        loop_rate.sleep();
+        
+    
     }
 
     return 0;
